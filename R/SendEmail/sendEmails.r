490494a9e642c1f1f8a780d8a6695bbe54b3d238
zumansky <zohra.umansky@producthealth.com>
2015-06-28 16:28:55 +0100

    Add repo for R scripts

---
+++ R/SendEmail/sendEmails.r
@@ -0,0 +1,56 @@
+#install the mailR package
+install.packages('mailR')
+#load the library
+library('mailR')
+
+#================================================================#
+# testing the mailR package - sending an email with attached pdf #
+#================================================================#
+
+#set the pseudo random number generator seed so we can get the same results
+#generate 1000 gaussian/uniform numbers
+set.seed(1); gNum <- rnorm(1000)
+set.seed(1); uNum <- runif(1000)
+#path where the pdf will be stored locally
+pdfFileName1 <- '/tmp/histoGauss.pdf'
+pdfFileName2 <- '/tmp/histoUnif.pdf'
+
+#plot the first pdf
+pdf(file = pdfFileName1)
+hist(gNum, main='gaussian distribution')
+graphics.off()
+
+#plot the second pdf
+pdf(file = pdfFileName2)
+hist(uNum, main='uniform distribution')
+graphics.off()
+
+#as of 20150607 this worked once we changed an option in gmail by removing extra security
+#there: https://www.google.com/settings/security/lesssecureapps
+send.mail(from = 'sender@gmail.com',
+          to = c('recipient@gmail.com'),
+          subject = 'Subject of my email',
+          body = 'Body of my email',
+          smtp = list(host.name = 'smtp.gmail.com', port = 465, 
+                      user.name = 'sender@gmail.com', passwd = 'senderpassword', 
+                      ssl = TRUE),
+          authenticate = TRUE,
+          send = TRUE,
+          attach.files = c(pdfFileName1, pdfFileName2),
+          file.names = c('file1.pdf', 'file2.pdf') # optional parameter
+)
+
+#as of 20150607 this is not working
+send.mail(debug = TRUE,
+          from = 'sender@gmail.com',
+          to = c('sender@gmail.com'),
+          subject = "Subject of the email",
+          body = "Body of the email",
+          smtp = list(host.name = "aspmx.l.google.com", port = 25, ssl=FALSE),
+          authenticate = FALSE,
+          send = TRUE,
+          attach.files = c(pdfFileName1, pdfFileName2),
+          file.names = c('file1.pdf', 'file2.pdf') # optional parameter
+)
+
+
